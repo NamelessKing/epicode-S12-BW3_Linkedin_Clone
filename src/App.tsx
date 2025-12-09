37c300d20c3ac67b0088a66e8873aac4654c86c8
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2025-12-09 16:02:05 +0100

    adds header crd

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css"
 // import './App.css'
 import './App.css'
 import TopNavbar from './components/layout/Navbar'
+import { useAppDispatch } from './store'
+import { useEffect } from 'react'
+
+import { fetchCurrentUser } from "./store/userSlice"
 
 function App() {
+  const dispatch = useAppDispatch()
 
+  useEffect(() => {
+    dispatch(fetchCurrentUser())
+  }, [])
 
   return (
     <>
--- src/components/profile/ProfileHeaderCard.tsx
+++ src/components/profile/ProfileHeaderCard.tsx
@@ -1,50 +1,62 @@
-import { Container, Col, Row, Card, Button, Badge } from "react-bootstrap";
+import { Container, Col, Row, Card, Button, Badge } from "react-bootstrap"
 import { LuPencil } from "react-icons/lu"
+import { useAppSelector } from "../../store"
 
 const ProfileHeaderCard = () => {
+  const user = useAppSelector((state) => state.user.data)
+
   return (
     <Container className="my-3">
       <Card>
         <Row className="position-relative">
           <Card.Img variant="top" src="https://placecats.com/400/100" />
 
-            <Badge pill className="position-absolute w">
-              <LuPencil />
-            </Badge>
+          <Badge pill className="position-absolute w">
+            <LuPencil />
+          </Badge>
         </Row>
 
         <Row className="position-absolute">
-          <img className="rounded-circle border-2" />
+          <img src={user?.image} className="rounded-circle border-2 w-25" />
         </Row>
         <Card.Body>
           <Row>
             <Col>
-              <Card.Title></Card.Title>
-              <Card.Text></Card.Text>
-              <Card.Text></Card.Text>
+              <Card.Title>
+                {user?.name} {user?.surname}
+              </Card.Title>
+              <Card.Text>{user?.area}</Card.Text>
             </Col>
             <Col>
               <Row>
                 <img className="rounded-circle" />
-                <a href=""></a>
+                <a href="">{user?.title}</a>
               </Row>
               <Row>
                 <img className="rounded-circle" />
-                <a href=""></a>
+                <a href="">{user?.email}</a>
               </Row>
             </Col>
           </Row>
 
           <Row>
-            <Button variant="primary">Disponibile per</Button>
-            <Button variant="primary">Aggiungi sezione del profilo</Button>
-            <Button variant="primary">Migliora profilo</Button>
-            <Button variant="primary">Risorse</Button>
+            <Button variant="primary" className="w-25">
+              Disponibile per
+            </Button>
+            <Button variant="primary" className="w-25">
+              Aggiungi sezione del profilo
+            </Button>
+            <Button variant="primary" className="w-25">
+              Migliora profilo
+            </Button>
+            <Button variant="primary" className="w-25">
+              Risorse
+            </Button>
           </Row>
         </Card.Body>
       </Card>
     </Container>
   )
-};
+}
 
-export default ProfileHeaderCard;
+export default ProfileHeaderCard
