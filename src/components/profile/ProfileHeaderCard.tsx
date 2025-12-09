1bd2a5604512da785aef546281a756f172c1b5c6
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2025-12-09 15:11:04 +0100

    layout & profile header set

--- src/components/profile/ProfileHeaderCard.tsx
+++ src/components/profile/ProfileHeaderCard.tsx
@@ -1,10 +1,50 @@
+import { Container, Col, Row, Card, Button, Badge } from "react-bootstrap";
+import { LuPencil } from "react-icons/lu"
+
 const ProfileHeaderCard = () => {
   return (
-    <div>
-      {/* Profile header card placeholder */}
-      <h2>Profile Header Card</h2>
-    </div>
-  );
+    <Container className="my-3">
+      <Card>
+        <Row className="position-relative">
+          <Card.Img variant="top" src="https://placecats.com/400/100" />
+
+            <Badge pill className="position-absolute w">
+              <LuPencil />
+            </Badge>
+        </Row>
+
+        <Row className="position-absolute">
+          <img className="rounded-circle border-2" />
+        </Row>
+        <Card.Body>
+          <Row>
+            <Col>
+              <Card.Title></Card.Title>
+              <Card.Text></Card.Text>
+              <Card.Text></Card.Text>
+            </Col>
+            <Col>
+              <Row>
+                <img className="rounded-circle" />
+                <a href=""></a>
+              </Row>
+              <Row>
+                <img className="rounded-circle" />
+                <a href=""></a>
+              </Row>
+            </Col>
+          </Row>
+
+          <Row>
+            <Button variant="primary">Disponibile per</Button>
+            <Button variant="primary">Aggiungi sezione del profilo</Button>
+            <Button variant="primary">Migliora profilo</Button>
+            <Button variant="primary">Risorse</Button>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Container>
+  )
 };
 
 export default ProfileHeaderCard;
--- src/components/profile/ProfilePage.tsx
+++ src/components/profile/ProfilePage.tsx
@@ -1,10 +1,30 @@
+import { Container, Row, Col } from "react-bootstrap";
+import ProfileHeaderCard from "./ProfileHeaderCard";
+
 const ProfilePage = () => {
   return (
-    <div>
-      {/* Profile page wrapper */}
-      <h1>Profile Page</h1>
-    </div>
-  );
+      <Container fluid>
+        <Row className="justify-content-center w-75 mx-auto">
+          <Col xs={12} md={9}>
+            <ProfileHeaderCard />
+            {/* <AnalisysBox />
+            <InfoComponent />
+            <Services />
+            <Activities />
+            <Experience />
+            <Education />
+            <Certificates />
+            <Interests /> */}
+          </Col>
+          <Col xs={12} md={3}>
+            {/* <Settings />
+            <OtherProfiles />
+            <Suggested /> */}
+          </Col>
+        </Row>
+      </Container>
+
+  )
 };
 
 export default ProfilePage;
