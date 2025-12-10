5c077d7faf955f808372ba968c0b5a00e50e62ff
pask517 <andreapascarella99@gmail.com>
2025-12-10 19:07:49 +0100

    updated

--- src/components/profile/ProfileAnalysis.tsx
+++ src/components/profile/ProfileAnalysis.tsx
@@ -1,25 +1,31 @@
-import { Card, Container, Row, Col } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap"
 import {
   BsEyeFill,
   BsFillPeopleFill,
   BsBarChartLineFill,
   BsSearch,
   BsArrowRight,
-} from "react-icons/bs";
+} from "react-icons/bs"
 
 const ProfileAnalysis = function () {
   return (
     <>
-    <Container className='my-3'>
-      <Card>
-        <Card.Body>
-          <Card.Title>Analisi</Card.Title>
-          <Card.Text><BsEyeFill className='me-1'/>Solo per te</Card.Text>
-          <Container>
-            <Row className='row-cols-3'>
-                <Col className='p-0'>
-                    <p><BsFillPeopleFill className='me-1'/> 1 visualizzazione del profilo</p>
-                    <p>Scopri chi ha visto il tuo profilo.</p>
+      <Container className="my-3">
+        <Card>
+          <Card.Body>
+            <Card.Title>Analisi</Card.Title>
+            <Card.Text>
+              <BsEyeFill className="me-1" />
+              Solo per te
+            </Card.Text>
+            <Container>
+              <Row className="row-cols-3">
+                <Col className="p-0">
+                  <p>
+                    <BsFillPeopleFill className="me-1" /> 1 visualizzazione del
+                    profilo
+                  </p>
+                  <p>Scopri chi ha visto il tuo profilo.</p>
                 </Col>
                 <Col>
                   <p>
@@ -34,17 +40,17 @@ const ProfileAnalysis = function () {
                   </p>
                   <p>Vedi quante volte compari nei risultati di ricerca</p>
                 </Col>
-            </Row>
-          </Container>
-        </Card.Body>
-          <hr className='my-2'/>
-          <Card.Text className='m-2'>
-            <h6 className='text-center'>Mostra tutte le analisi <BsArrowRight /></h6>
+              </Row>
+            </Container>
+          </Card.Body>
+          <hr className="my-2" />
+          <Card.Text as={"h6"} className="m-2 text-center">
+            Mostra tutte le analisi <BsArrowRight />
           </Card.Text>
-      </Card>
-    </Container>
+        </Card>
+      </Container>
     </>
-  );
-};
+  )
+}
 
-export default ProfileAnalysis;
+export default ProfileAnalysis
--- src/components/profile/ProfileExperiences.tsx
+++ src/components/profile/ProfileExperiences.tsx
@@ -1,6 +1,7 @@
-import { Row, Col, Container, Card } from "react-bootstrap"
+import { Row, Col, Container, Card, Form, Button } from "react-bootstrap"
 import { LuPencil, LuPlus } from "react-icons/lu"
 import { BsArrowRight } from "react-icons/bs"
+import { useState, useEffect } from "react"
 
 type ExperienceReference = {
   role: string
@@ -9,12 +10,12 @@ type ExperienceReference = {
   endDate: string
   description: string
   area: string
-  userName: string
-  image: string
-  createdAt: string
-  updatedAt: string
-  __v: number
-  _id: string
+  userName?: string
+  image?: string
+  createdAt?: string
+  updatedAt?: string
+  __v?: number
+  _id?: string
 }
 const consultedExperiences: ExperienceReference[] = [
   {
@@ -45,7 +46,7 @@ const consultedExperiences: ExperienceReference[] = [
     createdAt: "2023-06-16T19:58:31.019Z",
     updatedAt: "2023-06-16T19:58:31.019Z",
     __v: 0, // SERVER GENERATED
-    _id: "5d925e677360c41e0046d1f5", // SERVER GENERATED
+    _id: "5d925e677360c41e0046d1f6", // SERVER GENERATED
   },
   {
     role: "Full Stack Web Developer",
@@ -60,11 +61,39 @@ const consultedExperiences: ExperienceReference[] = [
     createdAt: "2023-06-16T19:58:31.019Z",
     updatedAt: "2023-06-16T19:58:31.019Z",
     __v: 0, // SERVER GENERATED
-    _id: "5d925e677360c41e0046d1f5", // SERVER GENERATED
+    _id: "5d925e677360c41e0046d1f7", // SERVER GENERATED
   },
 ]
 
 const ProfileExperiences = function () {
+  const [esperienze, setEsperienze] = useState<ExperienceReference[]>([])
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    setEsperienze(consultedExperiences)
+  }, [])
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e?.currentTarget)
+    const newExperience: ExperienceReference = {
+      role: formData.get("role") as string,
+      company: formData.get("company") as string,
+      startDate: formData.get("startDate") as string,
+      endDate: formData.get("endDate") as string,
+      area: formData.get("area") as string,
+      description: formData.get("description") as string,
+    }
+    setEsperienze([...esperienze, newExperience])
+    console.log(esperienze)
+  }
+  console.log(esperienze)
+  const handleShow = () => {
+    setShow(true)
+  }
+  const handleClose = () => {
+    setShow(false)
+  }
   return (
     <Container className="mb-3">
       <Card>
@@ -77,13 +106,54 @@ const ProfileExperiences = function () {
               <LuPlus
                 className="mt-2 me-3 fs-5"
                 style={{ cursor: "pointer" }}
+                onClick={() => {
+                  handleShow()
+                }}
               />
               <LuPencil className="mt-2" style={{ cursor: "pointer" }} />
             </Col>
           </Row>
-          {consultedExperiences.map((experience, i) => (
-            <>
-              <Row key={experience._id}>
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3 d-flex flex-row flex-wrap justify-content-around ">
+              <Col className="mb-2" xs={12} md={5}>
+                <Form.Label>Ruolo</Form.Label>
+                <Form.Control type="textarea" name="role" />
+              </Col>
+              <Col className="mb-2" xs={12} md={5}>
+                <Form.Label>Compagnia</Form.Label>
+                <Form.Control type="textarea" name="company" />
+              </Col>
+              <Col className="mb-2" xs={12} md={5}>
+                <Form.Label>Data di inizio</Form.Label>
+                <Form.Control type="textarea" name="startDate" />
+              </Col>
+              <Col className="mb-2" xs={12} md={5}>
+                <Form.Label>Data di fine</Form.Label>
+                <Form.Control type="textarea" name="endDate" />
+              </Col>
+              <Col className="mb-2" xs={12} md={5}>
+                <Form.Label>Luogo</Form.Label>
+                <Form.Control type="textarea" name="area" />
+              </Col>
+              <Col className="mb-2" xs={12} md={5}>
+                <Form.Label>Descrizione</Form.Label>
+                <Form.Control type="textarea" name="description" />
+              </Col>
+              <Col xs={12} className="text-center">
+                <Button
+                  type="submit"
+                  className="bg-primary text-light px-3 m-0"
+                >
+                  Submit
+                </Button>
+              </Col>
+            </Form.Group>
+          </Form>
+
+          {esperienze.map((experience, i) => (
+            <Container key={experience._id}>
+              <Row>
                 <Col md={2}>
                   <img src={experience.image} alt="" />
                 </Col>
@@ -99,13 +169,13 @@ const ProfileExperiences = function () {
                   <small className="">{experience.description}</small>
                 </Col>
               </Row>
-              {i < consultedExperiences.length - 1 ? <hr /> : ""}
-            </>
+              {i < esperienze.length - 1 ? <hr /> : ""}
+            </Container>
           ))}
         </Card.Body>
         <hr className="m-0 p-0" />
         <h6 className="text-center my-3" style={{ cursor: "pointer" }}>
-          Mostra tutte le esperienze({consultedExperiences.length})&nbsp;
+          Mostra tutte le esperienze({esperienze.length})&nbsp;
           <BsArrowRight />
         </h6>
       </Card>
