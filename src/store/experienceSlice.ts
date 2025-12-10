1da6de2162622e9887eec8e9ea59be9a3cd08c59
Leon35-fc <leon.f35@alice.it>
2025-12-10 12:27:36 +0100

    experienceSlice

---
+++ src/store/experienceSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import type { Experience } from "../types/experience";
+import { fetchAllExperiences, createExperience, fetchExperience, updateExperience, deleteExperience} from "../api/expirienceApiApi";
+
+interface ExperiencesState{
+    data: Experience[];
+    loading: boolean;
+    error: string | null;
+}
+
+const experienceInitialState: ExperiencesState = {
+    data: [],
+    loading: false,
+    error: null
+}
+
