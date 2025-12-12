9850c553d7819c6c3beaad28ec1cb4791f0d4773
Leon35-fc <leon.f35@alice.it>
2025-12-12 12:45:01 +0100

    Jobs post ok

--- src/components/jobs/JobsMainContent.tsx
+++ src/components/jobs/JobsMainContent.tsx
@@ -1,58 +1,110 @@
-import { Card, Button } from "react-bootstrap";
-import "./JobsStyles.css"; // 👈 IMPORTANTE
+import { Card, Button, Form, CardText, CardTitle } from 'react-bootstrap';
+import './JobsStyles.css'; // 👈 IMPORTANTE
+import { useEffect, useState } from 'react';
 
-const staticJobs = [
-  {
-    title: "Frontend Developer",
-    company: "Netflix",
-    location: "Milano, Lombardia, Italia",
-  },
-  {
-    title: "React Engineer",
-    company: "Amazon",
-    location: "Torino, Piemonte, Italia",
-  },
-  {
-    title: "Junior Web Developer",
-    company: "EPICODE",
-    location: "Remote",
-  },
-];
+// const staticJobs = [
+//   {
+//     title: "Frontend Developer",
+//     company: "Netflix",
+//     location: "Milano, Lombardia, Italia",
+//   },
+//   {
+//     title: "React Engineer",
+//     company: "Amazon",
+//     location: "Torino, Piemonte, Italia",
+//   },
+//   {
+//     title: "Junior Web Developer",
+//     company: "EPICODE",
+//     location: "Remote",
+//   },
+// ];
+
+const URL = 'https://strive-benchmark.herokuapp.com/api/jobs?search=';
 
 const JobsMainContent = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [fetchData, setFetchData] = useState([]);
+
+  useEffect(() => {
+    const getJobs = () => {
+      fetch(URL + searchQuery)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Errore nel recuper dei jobs' + response.status);
+          }
+        })
+        .then((data) => {
+          setFetchData(data.data.slice(0, 30));
+          console.log(data.data);
+        })
+        .catch((error) => console.log('ERRORE', error));
+    };
+
+    getJobs();
+  }, [searchQuery]);
+
   return (
-    <Card className="jobs-card shadow-sm">
-      {/* HEADER */}
-      <div className="mb-4">
-        <h5 className="fw-bold mb-2">Le principali offerte di lavoro per te</h5>
-        <p className="text-muted small mb-0">
-          In base al tuo profilo, alle tue preferenze e ad attività come
-          candidature, ricerche e salvataggi.
-        </p>
-      </div>
+    <>
+      <Form.Group>
+        <Form.Control
+          type="search"
+          placeholder="Cerca un lavoro"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="mb-3"
+        />
+      </Form.Group>
 
-      {/* LISTA JOBS */}
-      <div className="border-top pt-3">
-        {staticJobs.map((job, index) => (
-          <div key={index} className="jobs-job-item">
-            <h6 className="jobs-job-title">{job.title}</h6>
-            <p className="jobs-company">{job.company}</p>
-            <p className="jobs-location">{job.location}</p>
+      <Card className="jobs-card shadow-sm">
+        {/* HEADER */}
+        <div className="mb-4">
+          <h5 className="fw-bold mb-2">
+            Le principali offerte di lavoro per te
+          </h5>
+          <p className="text-muted small mb-0">
+            In base al tuo profilo, alle tue preferenze e ad attività come
+            candidature, ricerche e salvataggi.
+          </p>
+        </div>
 
-            <Button
-              variant="outline-primary"
-              size="sm"
-              className="jobs-visit-btn"
-            >
-              Salva
-            </Button>
-          </div>
-        ))}
-      </div>
-    </Card>
+        {/* LISTA JOBS */}
+        <div className="border-top pt-3">
+          {fetchData.map((job) => (
+            <div key={job._id} className="jobs-job-item">
+              <Card.Title className="jobs-job-title">{job.title}</Card.Title>
+              <Card.Subtitle>{job.publication_date}</Card.Subtitle>
+              <Card.Text className="jobs-company">
+                <div>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: job.description
+                        .replace(/&lt;/g, '<')
+                        .replace(/&gt;/g, '>')
+                    }}
+                  />
+                </div>
+              </Card.Text>
+              <Card.Text className="jobs-company">{job.company}</Card.Text>
+              <Card.Text className="jobs-location">
+                {job.candidate_required_location}
+              </Card.Text>
+
+              <Button
+                variant="outline-primary"
+                size="sm"
+                className="jobs-visit-btn"
+              >
+                Salva
+              </Button>
+            </div>
+          ))}
+        </div>
+      </Card>
+    </>
   );
 };
 
 export default JobsMainContent;
-
-
--- src/components/jobs/JobsSidebar.tsx
+++ src/components/jobs/JobsSidebar.tsx
@@ -1,23 +1,50 @@
-import { Card, ListGroup } from "react-bootstrap";
-import "./JobsStyles.css";
-
+import { Card, ListGroup } from 'react-bootstrap';
+import './JobsStyles.css';
+import { useAppSelector } from '../../store';
 const JobsSidebar = () => {
+  const currentUser = useAppSelector((state) => state.user.data);
+  console.log('', currentUser);
+
   return (
     <div>
       <Card className="jobs-sidebar-card mb-3 shadow-sm">
+          <div
+            className="d-flex justify-content-center"
+            style={{ marginTop: '-36px' }}
+          >
+            <img
+              src={currentUser?.image}
+              alt="immagine del profilo"
+              width={72}
+              height={72}
+              className="rounded-circle border border-2 border-white"
+            />
+          </div>
+          {/* <div className="rounded-circle jobs-profile-placeholder mb-3"></div> */}
         <Card.Body className="text-center">
-          <div className="rounded-circle jobs-profile-placeholder mb-3"></div>
-          <h6 className="fw-bold mb-1">Il tuo profilo</h6>
-          <p className="text-muted small">Completa il profilo per migliori offerte</p>
+          <h6 className="fw-bold mb-1">
+            {currentUser?.name} {currentUser?.surname}
+          </h6>
+          <p className="text-muted small">
+            Completa il profilo per migliori offerte
+          </p>
         </Card.Body>
       </Card>
 
       <Card className="jobs-sidebar-card shadow-sm">
         <ListGroup variant="flush">
-          <ListGroup.Item className="jobs-menu-item">Offerte salvate</ListGroup.Item>
-          <ListGroup.Item className="jobs-menu-item">Candidature</ListGroup.Item>
-          <ListGroup.Item className="jobs-menu-item">Avvisi lavoro</ListGroup.Item>
-          <ListGroup.Item className="jobs-menu-item">Aziende che segui</ListGroup.Item>
+          <ListGroup.Item className="jobs-menu-item">
+            Offerte salvate
+          </ListGroup.Item>
+          <ListGroup.Item className="jobs-menu-item">
+            Candidature
+          </ListGroup.Item>
+          <ListGroup.Item className="jobs-menu-item">
+            Avvisi lavoro
+          </ListGroup.Item>
+          <ListGroup.Item className="jobs-menu-item">
+            Aziende che segui
+          </ListGroup.Item>
         </ListGroup>
       </Card>
     </div>
@@ -25,4 +52,3 @@ const JobsSidebar = () => {
 };
 
 export default JobsSidebar;
-
